076c27fc0d6be4aad5def821372589b26d869768
Venkat Polumahanti <venkat4541@gmail.com>
2017-03-11 19:41:32 -0500

    Maintaining a session using JWT and logout a user without a valid token.

--- app.js
+++ app.js
@@ -26,7 +26,39 @@ angular
         controller: 'profileController as user'
       });
 
+      function redirect($q, $injector, store, $location) {
+        return {
+          responseError: function(rejection) {
+            if(rejection.status === 401) {
+              var auth = $injector.get('auth');
+              auth.signout();
+              store.remove('profile');
+              store.remove('id_token');
+              $location.path('/home');
+            }
+            return $q.reject(rejection);
+          }
+        }
+      }
+
+      $provide.factory('redirect', redirect);
+      $httpProvider.interceptors.push('redirect');
       $httpProvider.interceptors.push('jwtInterceptor');
+  })
+  .run(function($rootScope, auth, store, jwtHelper, $location) {
+    $rootScope.$on('$locationChangeStart', function() {
+      var token = store.get('id_token');
+      if(token) {
+        if(!jwtHelper.isTokenExpired(token)) {
+          if(!auth.isAuthenticated) {
+            auth.authenticate(store.get('profile'), token);
+          }
+        }
+      } else {
+        console.log('Not logged In');
+        $location.path('/home');
+      }
+    })
   })
   .config(function Config($httpProvider, jwtOptionsProvider) {
     jwtOptionsProvider.config({
